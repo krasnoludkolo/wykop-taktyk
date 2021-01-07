070653f2aa319161a18ffee4604e33982b9575fd
danek <janek@projmen.pl>
2021-01-07 13:02:37 +0100

    Read keys from file

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import time
 import logging
-from typing import NoReturn
+from typing import NoReturn, Tuple
 from wykop import WykopAPI
 
 from ReminderRepository import ShelveReminderRepository
 from TaktykBot import TaktykBot
+
+KEYS_FILE_NAME = 'keys'
 
 
 def main_loop(bot: TaktykBot):
@@ -26,10 +28,23 @@ def main() -> NoReturn:
         time.sleep(1)
 
 
-def create_wykop_api():
+def read_keys_from_file() -> Tuple[str, ...]:
+    with open(KEYS_FILE_NAME) as f:
+        return tuple(f.readline().split())
+
+
+def read_keys_from_envs() -> Tuple[str, str, str]:
     key = os.environ.get('WYKOP_TAKTYK_KEY')
     secret = os.environ.get('WYKOP_TAKTYK_SECRET')
     account_key = os.environ.get('WYKOP_TAKTYK_ACCOUNTKEY')
+    return key, secret, account_key
+
+
+def create_wykop_api():
+    if os.path.isfile(KEYS_FILE_NAME):
+        key, secret, account_key = read_keys_from_file()
+    else:
+        key, secret, account_key = read_keys_from_envs()
     api = WykopAPI(key, secret, account_key=account_key)
     api.authenticate()
     return api
