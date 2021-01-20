2b71fb1fa8825929cbcab553994c25adf92658e7
Danek <janek@projmen.pl>
2021-01-20 12:38:09 +0100

    Extract ReminderCandidate class

--- src/taktyk/model.py
+++ src/taktyk/model.py
@@ -20,4 +20,23 @@ class Reminder:
         return f'Reminder({self.logins_with_last_seen_comment_id}, {self.entry_id}, {self.comment_id}, {self.comments_count})'
 
 
+class ReminderCandidate:
+
+    def __init__(self, login: str, entry_id: str, comment_id: str, comments_count: int) -> None:
+        self.comment_id: str = comment_id
+        self.login: str = login
+        self.entry_id: str = entry_id
+        self.comment_id: str = comment_id
+        self.comments_count: int = comments_count
+
+    def __iter__(self):
+        return iter((self.login, self.entry_id, self.comment_id, self.comments_count))
+
+    def __str__(self) -> str:
+        return f'Reminder({self.login}, {self.entry_id}, {self.comment_id}, {self.comments_count})'
+
+    def __repr__(self) -> str:
+        return f'Reminder({self.login}, {self.entry_id}, {self.comment_id}, {self.comments_count})'
+
+
 entry_url = 'https://www.wykop.pl/wpis'
