d9ccff93b5c00a0dafc35b5343089a7b5a1f4c11
Danek <janek@projmen.pl>
2021-01-28 08:28:58 +0100

    extract str and repr method

--- src/taktyk/model.py
+++ src/taktyk/model.py
@@ -1,6 +1,10 @@
-from typing import Set, Dict
+from typing import Dict
+
+from taktyk.python_utils import auto_str, auto_repr
 
 
+@auto_str
+@auto_repr
 class Reminder:
 
     def __init__(self, logins_with_last_seen_comment_id: Dict[str, str], entry_id: str, comment_id: str,
@@ -9,17 +13,15 @@ class Reminder:
         self.comment_id: str = comment_id
         self.comments_count: int = comments_count
         self.logins_with_last_seen_comment_id: Dict[str, str] = logins_with_last_seen_comment_id
+        self.active = True
 
     def __iter__(self):
-        return iter((self.logins_with_last_seen_comment_id, self.entry_id, self.comment_id, self.comments_count))
-
-    def __str__(self) -> str:
-        return f'Reminder({self.logins_with_last_seen_comment_id}, {self.entry_id}, {self.comment_id}, {self.comments_count})'
-
-    def __repr__(self) -> str:
-        return f'Reminder({self.logins_with_last_seen_comment_id}, {self.entry_id}, {self.comment_id}, {self.comments_count})'
+        return iter(
+            (self.logins_with_last_seen_comment_id, self.entry_id, self.comment_id, self.comments_count, self.active))
 
 
+@auto_str
+@auto_repr
 class ReminderCandidate:
 
     def __init__(self, login: str, entry_id: str, comment_id: str, comments_count: int) -> None:
@@ -31,12 +33,3 @@ class ReminderCandidate:
 
     def __iter__(self):
         return iter((self.login, self.entry_id, self.comment_id, self.comments_count))
-
-    def __str__(self) -> str:
-        return f'Reminder({self.login}, {self.entry_id}, {self.comment_id}, {self.comments_count})'
-
-    def __repr__(self) -> str:
-        return f'Reminder({self.login}, {self.entry_id}, {self.comment_id}, {self.comments_count})'
-
-
-
