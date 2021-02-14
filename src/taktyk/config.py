7849ae5db706d3790ca56c9f49063bd335d7bb77
Danek <janek@projmen.pl>
2021-02-14 08:30:16 +0100

    Add comment to explain why wykop app key can be stored in repo

--- src/taktyk/config.py
+++ src/taktyk/config.py
@@ -1,7 +1,7 @@
 import os
 
 KEYS_FILE_NAME = 'keys'
-WYKOP_APP_KEY = 'aNd401dAPp'
+WYKOP_APP_KEY = 'aNd401dAPp'  # official wykop app key (from android app)
 BASE_DIR = os.getcwd()
 LOG_DIR = f'{BASE_DIR}/logs'
 REPOSITORY_DIR = f'{BASE_DIR}/db'
